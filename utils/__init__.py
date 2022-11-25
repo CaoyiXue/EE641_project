4e5ad4c61cbaf5a610b741e2c51372dad5106951
caoyixue <caoyixue@usc.edu>
2022-11-24 20:41:57 -0800

    creat hdf5 and use it

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .TorchIODataloader import *
 from .running_loop import *
+from .HDF5Dataset import *
--- utils/running_loop.py
+++ utils/running_loop.py
@@ -1,15 +1,12 @@
 import time
 import torch
 import torchio as tio
-
+#label2num = {'COVID-19': 0, 'Non-COVID': 1, 'Normal': 2}
 def training_loop(model, optimizer, loss_fcn, 
-                  train_loader, val_loader, mask_name='lung mask', 
+                  train_loader, val_loader, mask_name='infection mask',
                   n_epochs=1, device="cpu",
                   save_path="model.pt", stop=2):
     
-    assert(train_loader.batch_size != 1 and val_loader.batch_size != 1)
-    assert(mask_name in ['lung mask', 'infection mask'])
-
     n_stop = 0
     val_loss_min = torch.inf
     train_size = len(train_loader.dataset)
@@ -17,19 +14,18 @@ def training_loop(model, optimizer, loss_fcn,
     for epoch in range(1, n_epochs+1):
         train_loss = 0.0
         model.train()
-        print(f"Epoch {epoch} Start:")
+        print(f"Epoch {epoch} Training Start:")
         start_time = time.time()
-        for batch_i, batch in enumerate(train_loader):
+        for batch_i, (imgs, masks, labels) in enumerate(train_loader):
 
             if batch_i % 10 == 0:
                 print(f"Epoch {epoch}, Batch {batch_i}")
-
-            imgs = batch['image'][tio.DATA].squeeze_(-1).to(device)
-            labels = batch[mask_name][tio.DATA].squeeze_().long().to(device)
+            imgs = imgs.to(device)
+            masks = masks.to(device)
             output = model(imgs)
 
             optimizer.zero_grad()
-            loss = loss_fcn(output, labels)
+            loss = loss_fcn(output, masks)
             loss.backward()
             optimizer.step()
 
@@ -44,18 +40,19 @@ def training_loop(model, optimizer, loss_fcn,
 
         # validation
         val_loss = 0.0
+        print(f"Epoch {epoch} Validation Start:")
         with torch.no_grad():
             model.eval()
-            for batch_i, batch in enumerate(val_loader):
+            for batch_i, (imgs, masks, labels) in enumerate(val_loader):
 
                 if batch_i % 100 == 0:
                     print(f"Epoch {epoch}, Batch {batch_i}")
 
-                imgs = batch['image']['data'].squeeze_(-1).to(device)
-                labels = batch[mask_name]['data'].squeeze_().long().to(device)
+                imgs = imgs.to(device)
+                masks = masks.to(device)
                 output = model(imgs)
 
-                loss = loss_fcn(output, labels)
+                loss = loss_fcn(output, masks)
                 val_loss += loss * imgs.shape[0]
         
             val_loss /= val_size
@@ -65,8 +62,7 @@ def training_loop(model, optimizer, loss_fcn,
 
             if val_loss < val_loss_min:
                     val_loss_min = val_loss
-                    model_scripted = torch.jit.script(model)
-                    model_scripted.save(save_path)
+                    torch.save(model, save_path)
                     print('Detect Improvement, Save Model')
             else:
                 n_stop += 1
@@ -86,12 +82,16 @@ def IoU(pred, real):
     intersection = (pred * real).sum()
     total = (pred + real).sum()
     union = total - intersection
+    if union == 0:
+        return 0.0
     return intersection/union
 
 def DSC(pred, real):
     pred = pred.view(-1)
     real = real.view(-1)
     intersection = (pred * real).sum()
+    if (pred.sum()+real.sum()) == 0:
+        return 0.0
     return (2.*intersection)/(pred.sum()+real.sum())
 
 def infection_rate(lung_mask, infection_mask):
@@ -100,7 +100,7 @@ def infection_rate(lung_mask, infection_mask):
     infect = (lung_mask * infection_mask).sum()
     return infect / lung_mask.sum()
 
-def test_loop(model, test_loader, mask_name='lung mask', device="cpu"):
+def test_loop(model, test_loader, mask_name='infection mask', device="cpu"):
 
     assert(test_loader.batch_size==1)
     assert(mask_name in ['lung mask', 'infection mask'])
@@ -110,18 +110,21 @@ def test_loop(model, test_loader, mask_name='lung mask', device="cpu"):
 
     with torch.no_grad():
         model.eval()
-        for batch_i, batch in enumerate(test_loader):
+        for batch_i, (img, mask, label) in enumerate(test_loader):
 
             if batch_i % 10 == 0:
                 print(f'patient {batch_i}')
 
-            imgs = batch['image'][tio.DATA].squeeze_(-1).to(device)
-            labels = batch[mask_name][tio.DATA].squeeze_().long().to(device)
-            outputs = model(imgs)
+            if mask_name == 'infection mask' and label != 0:
+                continue
+            
+            img = img.to(device)
+            mask = mask.to(device)
+            outputs = model(img)
             _, predicted = torch.max(outputs, dim=1)
-            acc_tensor[batch_i] = accuracy(predicted, labels)
-            IoU_tensor[batch_i] = IoU(predicted, labels)
-            DSC_tensor[batch_i] = DSC(predicted, labels)
+            acc_tensor[batch_i] = accuracy(predicted, mask)
+            IoU_tensor[batch_i] = IoU(predicted, mask)
+            DSC_tensor[batch_i] = DSC(predicted, mask)
 
     acc_mean, acc_std = acc_tensor.mean().item(), acc_tensor.std().item()
     IoU_mean, IoU_std = IoU_tensor.mean().item(), IoU_tensor.std().item()
